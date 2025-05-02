387ae34c55312cffbf2afcbc5822c2eec86ed3e9
andres-melgar <amelgar@pucp.edu.pe>
2025-05-02 09:03:43 -0500

    clase

---
+++ 0682/sesión12 - WA/SoftInv/SoftInvWA/Prueba.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SoftInv.Model;
+using SoftInvBusiness;
+
+namespace SoftInvWA
+{
+    public partial class Prueba : System.Web.UI.Page
+    {
+        private AlmacenBO almacenBO;
+        private BindingList<AlmacenesDTO> listaAlmacenes;
+
+        public Prueba()
+        {
+            this.AlmacenBO = new AlmacenBO();
+            this.ListaAlmacenes = this.AlmacenBO.ListarTodos();
+        }
+
+        public AlmacenBO AlmacenBO { get => almacenBO; set => almacenBO = value; }
+        public BindingList<AlmacenesDTO> ListaAlmacenes { get => listaAlmacenes; set => listaAlmacenes = value; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
