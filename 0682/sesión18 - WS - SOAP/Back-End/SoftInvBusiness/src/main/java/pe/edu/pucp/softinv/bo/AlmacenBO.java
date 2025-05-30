361f4b4a4f11bb21343b2f873ed27e1bc630ff02
andres-melgar <amelgar@pucp.edu.pe>
2025-05-30 07:57:58 -0500

    clase

--- 0682/sesión18 - WS - SOAP/Back-End/SoftInvBusiness/src/main/java/pe/edu/pucp/softinv/bo/AlmacenBO.java
+++ 0682/sesión18 - WS - SOAP/Back-End/SoftInvBusiness/src/main/java/pe/edu/pucp/softinv/bo/AlmacenBO.java
@@ -1,5 +1,6 @@
 package pe.edu.pucp.softinv.bo;
 
+import java.util.ArrayList;
 import pe.edu.pucp.softinv.dao.AlmacenDAO;
 import pe.edu.pucp.softinv.daoImp.AlmacenDAOImpl;
 import pe.edu.pucp.softinv.model.AlmacenesDTO;
@@ -17,5 +18,27 @@ public class AlmacenBO {
         almacenesDTO.setNombre(nombre);
         almacenesDTO.setAlmacen_central(almacen_central);
         return this.almacenDAO.insertar(almacenesDTO);
+    }
+    
+    public AlmacenesDTO obtenerPorId(Integer almacenId){
+        return this.almacenDAO.obtenerPorId(almacenId);
+    }
+    
+    public ArrayList<AlmacenesDTO> listarTodos(){
+        return this.almacenDAO.listarTodos();
+    }
+    
+    public Integer modificar(Integer almacenId, String nombre, Boolean almacen_central){
+        AlmacenesDTO almacenesDTO = new AlmacenesDTO();
+        almacenesDTO.setAlmacenId(almacenId);
+        almacenesDTO.setNombre(nombre);
+        almacenesDTO.setAlmacen_central(almacen_central);
+        return this.almacenDAO.modificar(almacenesDTO);
+    }
+    
+    public Integer eliminar(Integer almacenId){
+        AlmacenesDTO almacenesDTO = new AlmacenesDTO();
+        almacenesDTO.setAlmacenId(almacenId);
+        return this.almacenDAO.eliminar(almacenesDTO);
     }
 }
