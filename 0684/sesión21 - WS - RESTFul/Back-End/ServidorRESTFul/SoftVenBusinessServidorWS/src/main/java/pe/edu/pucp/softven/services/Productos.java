1b77c4c98d99c41b1f105707898cc0c9d80f9cbb
andres-melgar <amelgar@pucp.edu.pe>
2025-06-09 08:09:52 -0500

    clase

--- 0684/sesión21 - WS - RESTFul/Back-End/ServidorRESTFul/SoftVenBusinessServidorWS/src/main/java/pe/edu/pucp/softven/services/Productos.java
+++ 0684/sesión21 - WS - RESTFul/Back-End/ServidorRESTFul/SoftVenBusinessServidorWS/src/main/java/pe/edu/pucp/softven/services/Productos.java
@@ -1,8 +1,10 @@
 package pe.edu.pucp.softven.services;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -22,25 +24,44 @@ public class Productos {
     public Productos() {
         this.productoBO = new ProductoBO();
     }
-    
+
     @GET
-    public ArrayList<ProductosDTO> listarTodos(){
+    public ArrayList<ProductosDTO> listarTodos() {
         return this.productoBO.listarTodos();
     }
-    
+
     @GET
     @Path("{id}")
-    public Response obtenerPorId(@PathParam("id") Integer productoId){
-        ProductosDTO producto= this.productoBO.obtenerPorId(productoId);
-        if (producto==null){
+    public Response obtenerPorId(@PathParam("id") Integer productoId) {
+        ProductosDTO producto = this.productoBO.obtenerPorId(productoId);
+        if (producto == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
         return Response.ok(producto).build();
     }
-    
+
     @POST
-    public Response insertar(ProductosDTO productosDTO){
+    public Response insertar(ProductosDTO productosDTO) {
         productosDTO = this.productoBO.insertar(productosDTO);
         return Response.status(Response.Status.CREATED).entity(productosDTO).build();
+    }
+
+    @PUT
+    public Response modificar(ProductosDTO productosDTO) {
+        Integer respuesta = this.productoBO.modificar(productosDTO);
+        if (respuesta > 0) {
+            return Response.ok(productosDTO).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Response eliminar(@PathParam("id") Integer productoId) {
+        Integer respuesta = this.productoBO.eliminar(productoId);
+        if (respuesta > 0) {
+            return Response.noContent().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
     }
 }
