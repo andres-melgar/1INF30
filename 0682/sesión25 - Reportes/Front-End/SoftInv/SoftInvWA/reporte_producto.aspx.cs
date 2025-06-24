b0cf08c7262f6373de0a604cbc032aedf4ad66e9
andres-melgar <amelgar@pucp.edu.pe>
2025-06-24 08:36:37 -0500

    clase

---
+++ 0682/sesión25 - Reportes/Front-End/SoftInv/SoftInvWA/reporte_producto.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SoftInv.BO;
+
+namespace SoftInvWA
+{
+    public partial class reporte_producto : System.Web.UI.Page
+    {
+        private AlmacenBO almacenBO;
+
+        public reporte_producto()
+        {
+            this.almacenBO = new AlmacenBO();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            byte[] reporte = this.almacenBO.reporteProductos();
+            this.almacenBO.abrirReporte(Response, "ReporteVendedores", reporte);
+        }
+    }
+}
