b0745c89a24bdf8ac1e6e5c390512de2abb7b304
Andrés Melgar <amelgar@pucp.edu.pe>
2025-08-18 22:57:49 -0500

    clase

---
+++ 0682/C#/HolaMundo_Consola/HolaMundo.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HolaMundo_VSC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
