c2a337b64ccf5d3ac9281e4227a481938117c1a2
Andrés Melgar <amelgar@pucp.edu.pe>
2025-08-25 09:28:46 -0500

    clase

---
+++ 0682/Sesión 02 - POO/Publicaciones_v01/Publicaciones_v01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Publicacion publicacion = new Publicacion("A relational model of data for large shared data banks", "E. F. Codd", 1970);
+            string[] autores = { "Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides" };
+            Publicacion publicacion = new Publicacion("Design Patterns: Elements of Reusable Object-Oriented Software ", autores, 1994);
+
+            //Son equivalentes
+            Console.WriteLine(publicacion);
+            //Console.WriteLine(publicacion.ToString());
+            autores[0] = "Andrés Melgar";
+            autores[1] = "Rony Cueva";
+            Console.WriteLine(publicacion);
+
+            publicacion.Nombre = "Diseño de patrones";
+            Console.WriteLine(publicacion.Referenciar());
+        }
+    }
+}
---
+++ 0682/Sesión 02 - POO/Publicaciones_v01/Publicaciones_v01/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Publicaciones_v01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Publicaciones_v01")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("540ece65-4382-4049-8581-dc7bfab86304")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
