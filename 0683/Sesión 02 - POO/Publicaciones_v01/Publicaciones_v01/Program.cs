000b113f02ca9223d76e8207e7277f0b3bca65a8
Andrés Melgar <amelgar@pucp.edu.pe>
2025-08-26 11:22:07 -0500

    clase

---
+++ 0683/Sesión 02 - POO/Publicaciones_v01/Publicaciones_v01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Publicacion publicacion = new Publicacion("A Relational Model of Data for Large Shared Data Banks", "Codd", 1970);            
+            string [] autores = new string[] { "Gamma", "Helm", "Johnson", "Vlissides" };
+            Publicacion publicacion = new Publicacion("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1994);
+            Console.WriteLine(publicacion);
+            //Console.WriteLine(publicacion.ToString());
+            //publicacion.Referenciar();
+            autores[0] = "Melgar";
+            autores[1] = "Cueva";
+            Console.WriteLine(publicacion);
+        }
+    }
+}
---
+++ 0683/Sesión 02 - POO/Publicaciones_v02/Publicaciones_v02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Publicaciones_v02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] autores = new string[] { "Gamma", "Helm", "Johnson", "Vlissides" };
+            Publicacion publicacion = new Publicacion("Design Patterns: Elements of Reusable Object-Oriented Software", autores, 1994);
+            Console.WriteLine(publicacion);
+            autores[0] = "Melgar";
+            autores[1] = "Cueva";
+            Console.WriteLine(publicacion);
+        }
+    }
+}
