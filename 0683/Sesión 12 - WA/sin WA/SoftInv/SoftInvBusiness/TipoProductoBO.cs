e9f72e939909de65f776b537721e75ed118a58ff
Andrés Melgar <amelgar@pucp.edu.pe>
2025-09-24 09:56:26 -0500

    preparación de clase

---
+++ 0683/Sesión 12 - WA/sin WA/SoftInv/SoftInvBusiness/TipoProductoBO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftInv.Model;
+using SoftInv.DAO;
+using SoftInv.DAOImpl;
+
+namespace SoftInv.BO
+{
+    public class TipoProductoBO
+    {
+        private TipoProductoDAO tipoProductoDAO;
+
+        public TipoProductoBO()
+        {
+            this.tipoProductoDAO = new TipoProductoDAOImpl();
+        }
+
+        public IList<TiposProductosDTO> ConsultarTiposDeProductos(string nombre)
+        {
+            return this.tipoProductoDAO.ConsultarTiposDeProductos(nombre);
+        }
+
+        public TiposProductosDTO ObtenerPorId(int tipoProductoId)
+        {
+            return this.tipoProductoDAO.ObtenerPorId(tipoProductoId);
+        }
+    }
+}
