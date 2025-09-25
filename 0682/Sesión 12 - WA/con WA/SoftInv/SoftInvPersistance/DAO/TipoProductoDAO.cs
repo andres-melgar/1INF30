6efc205924531439a65a639cf7e764ff8cd11fde
Andrés Melgar <amelgar@pucp.edu.pe>
2025-09-25 09:01:22 -0500

    clase

---
+++ 0682/Sesión 12 - WA/con WA/SoftInv/SoftInvPersistance/DAO/TipoProductoDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftInv.Model;
+
+namespace SoftInv.DAO
+{
+    public interface TipoProductoDAO
+    {
+        IList<TiposProductosDTO> ConsultarTiposDeProductos(string nombre);
+
+        TiposProductosDTO ObtenerPorId(int tipoProductoId);
+    }
+}
