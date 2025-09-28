5df0080d93bf40757a2212aafbba30afe7935bee
Andrés Melgar <amelgar@pucp.edu.pe>
2025-09-27 21:54:22 -0500

    preparación de clases

---
+++ 0682/sesión13 - MasterPage+CRUD/sin CRUD/SoftInv/SoftInvDBManager/Util/MotorDeBaseDeDatos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftInv.Db.Util
+{
+    public enum MotorDeBaseDeDatos
+    {
+        MYSQL, MSSQL
+    }
+}
