efc136ea1ef3a801b13af7e46026044659199ee3
Andrés Melgar <amelgar@pucp.edu.pe>
2025-10-02 10:50:46 -0500

    clase

---
+++ 0683/sesión 14 - MasterPage+CRUD/SoftInv/SoftInvWA/almacen_listado.aspx.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SoftInvWA
+{
+
+
+    public partial class almacen_listado
+    {
+
+        /// <summary>
+        /// Control dgvAlmacen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgvAlmacen;
+
+        /// <summary>
+        /// Control btnInsertar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnInsertar;
+    }
+}
